005d121fea12c27e621eb80660f142c5147d25be
Dragan Kovacevic <kovacedr@sheridancollege.ca>
2016-08-03 12:16:31 -0400

    Added stuff to hoothoot class

--- LogicTier/HootHoot.cs
+++ LogicTier/HootHoot.cs
@@ -5,16 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using LogicTier;
 using ProjectProposal;
+using Windows.UI.Xaml.Controls;
 
 namespace LogicTier
 {
     public class HootHoot
     {
+        const float FallingSpeed = 9.81f; //The Falling speed of the bird is a fixed speed
 
-        //public void HootHoot(int position)
-        //{
+        Image OwlUi;
 
-        //}
+        double _startLocation;
+
+
+        /// <summary>
+        /// Hoot Hoot The Owl Character object init
+        /// </summary>
+        /// <param name="HootHootTheOwl"></param>
+        /// <param name="startLocation"></param>
+        public HootHoot(Image HootHootTheOwl)
+        {
+            OwlUi = HootHootTheOwl;
+
+
+        }
+
+
 
     }
 }
